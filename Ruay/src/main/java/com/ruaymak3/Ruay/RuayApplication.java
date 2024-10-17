3fab1b77ac29cbb4ddbedbb2607bce8bac5f9730
iamohmx_ <ohmzdev.contact@gmail.com>
2024-10-17 17:40:10 +0700

    Update somthing

---
+++ Ruay/src/main/java/com/ruaymak3/Ruay/RuayApplication.java
@@ -0,0 +1,13 @@
+package com.ruaymak3.Ruay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RuayApplication.class, args);
+	}
+
+}
---
+++ Ruay/src/test/java/com/ruaymak3/Ruay/RuayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruaymak3.Ruay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
